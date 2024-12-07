2022b9c366b8bbb93b70ba2a587044dfe6f23eaa
Mensart <scophield@outlook.com>
2024-12-07 04:56:58 +0000

    setting up apps  the necessary for the quizz App

---
+++ back_end/quizz_app/the_quiz_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TheQuizAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'the_quiz_app'
---
+++ frontEnd/QuizzFrontEnd/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Wellcome tho the quizz App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
